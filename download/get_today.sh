bb0576383d1edc4ce693d0446bb9784112a0aeee
Abe Handler <akh2103@gmail.com>
2020-03-30 15:00:47 -0400

    just backup

--- download/get_today.sh
+++ download/get_today.sh
@@ -1,3 +1,5 @@
 wget https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv -O latest.csv
 
-grep -f counties.txt latest.csv | grep Mass
+head -1 latest.csv > valley.csv
+
+grep -f counties.txt latest.csv | grep Mass >> valley.csv
