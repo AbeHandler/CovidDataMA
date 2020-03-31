2bb284b8bfa30fa92ac4a42637710bf0e53d49be
Abe Handler <akh2103@gmail.com>
2020-03-31 11:57:26 -0400

    just backup

--- plotting/plot.R
+++ plotting/plot.R
@@ -1,4 +1,4 @@
-valley <- read_csv("valley.csv")
+valley <- read_csv("plotting/valley.csv")
 
 ggplot(valley) + geom_line(aes(x=date, y=cases, color=county), size = 1.5) + scale_y_log10() + ggtitle("Confirmed COVID-19 cases by county, Pioneer Valley") + theme_light()
 
