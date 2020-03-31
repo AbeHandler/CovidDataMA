621f2f593040a64e5b512d9fb53e4423a6ada82f
Abe Handler <akh2103@gmail.com>
2020-03-31 12:21:30 -0400

    just backup

--- download/get_today.sh
+++ download/get_today.sh
@@ -1,7 +1,11 @@
+#!/usr/bin/env bash
+
 wget https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv -O plotting/latest.csv
 
 head -1 plotting/latest.csv > plotting/valley.csv
 
 grep -f download/counties.txt plotting/latest.csv | grep Mass >> plotting/valley.csv
+
+Rscript plotting/plot.R
 
 mv plotting/valley.png site/
