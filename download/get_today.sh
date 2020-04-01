45bb43db27cb66ed3340d0de866936e480885831
Abe Handler <akh2103@gmail.com>
2020-04-01 11:29:01 -0400

    just backup

--- download/get_today.sh
+++ download/get_today.sh
@@ -6,7 +6,7 @@ head -1 plotting/latest.csv > plotting/valley.csv
 
 grep -f download/counties.txt plotting/latest.csv | grep Mass >> plotting/valley.csv
 
-Rscript plotting/plot.R
+/Library/Frameworks/R.framework/Resources/bin/Rscript plotting/plot.R
 
 mv plotting/valley.png site/
 
