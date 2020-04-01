0e6eab5b434e571be509267d862eb53905fc4246
Abram Handler <ahandler@hobbes.cs.umass.edu>
2020-04-01 12:22:17 -0400

    fix

--- plotting/plot.R
+++ plotting/plot.R
@@ -2,6 +2,6 @@ library(tidyverse)
 
 valley <- read_csv("plotting/valley.csv")
 
-ggplot(valley) + geom_line(aes(x=date, y=cases, color=county), size = 1.5) + scale_y_log10() + ggtitle("Confirmed COVID-19 cases by county, Pioneer Valley") + theme_light()
+ggplot(valley) + geom_line(aes(x=date, y=cases, color=county), size = 1.5) + scale_y_log10() + ggtitle("Confirmed COVID-19 cases by county, Western MA") + theme_light()
 
 ggsave("plotting/valley.png")
