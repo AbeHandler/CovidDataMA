8c0a49329885f082f6fce55b51b0af6044a3279a
Abe Handler <akh2103@gmail.com>
2020-04-06 12:54:21 -0400

    just backup

--- plotting/make_table.r
+++ plotting/make_table.r
@@ -2,7 +2,7 @@ library(readr)
 library(tidyr)
 library(dplyr)
 options(digits=3)
-d=read_csv("latest.csv")
+d=read_csv("plotting/latest.csv")
 x = d %>% filter(state=='Massachusetts' & county %in% c("Hampshire","Hampden","Berkshire","Franklin")) %>% select(date,county,cases) %>% arrange(-as.integer(date)) %>% group_by(county) %>% summarise(now=date[1],now_cases=cases[1], lastweek=date[8], lastweek_cases=cases[8], weekly_growth=now_cases/lastweek_cases, daily_growth=weekly_growth^(1/7),doubling_time=log(2)/log(daily_growth))
 
 y=data.frame(1,1,1,1); names(y)=x$county
