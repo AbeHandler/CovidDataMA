1a0c0b0e30f83a4f3ce5e0866fb9082f3d734837
Abe Handler <akh2103@gmail.com>
2020-04-24 06:23:21 -0400

    just backup

--- plotting/changes.R
+++ plotting/changes.R
@@ -6,4 +6,4 @@ valley <- read_csv("plotting/changes.csv")
 
 ggplot(valley) + geom_line(aes(x=date, y=avg, color=county), size = 1.5) + xlab("day") + ylab("Average new confirmed cases") +  ggtitle("Average new confirmed COVID cases (over past 7 days)") + theme_light()
 
-ggsave("plotting/changes.png")
+ggsave("site/changes.png")
