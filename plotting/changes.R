e9836afa6dcab4f626c22f505a7ff30ea870ecc1
Abe Handler <akh2103@gmail.com>
2020-04-24 07:27:27 -0400

    just backup

--- plotting/changes.R
+++ plotting/changes.R
@@ -6,6 +6,10 @@ valley <- read_csv("plotting/changes.csv")
 
 valley$county <- factor(valley$county, levels = c("Berkshire", "Hampden", "Franklin", "Hampshire"))
 
-ggplot(valley) + geom_line(aes(x=date, y=avg, color=county), size = 1.5) + scale_color_manual(values = c("#1f77b4", "#FF7F0E", "#2CA02C", "#d62728")) + xlab("day") + ylab("Average new confirmed cases (over past 7 days)") +  ggtitle("Average new confirmed cases by county") + theme_light()
+ggplot(valley) + geom_line(aes(x=date, y=avg, color=county), size = 1.5) + scale_color_manual(values = c("#1f77b4", "#FF7F0E", "#2CA02C", "#d62728")) + ylab("Average new confirmed cases (over past 7 days)") + xlab("") + theme_minimal() + facet_grid(cols = vars(county)) + theme(strip.background = element_blank(),
+   strip.text.x = element_blank())
 
-ggsave("site/changes.png")
+width = 10
+height = (9/16) * width
+
+ggsave("site/changes.png", width = width, height=height)
