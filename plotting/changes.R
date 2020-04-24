98732c8112a42b5c7899cda1bd069302867aed32
Abe Handler <akh2103@gmail.com>
2020-04-24 07:07:00 -0400

    just backup

--- plotting/changes.R
+++ plotting/changes.R
@@ -4,6 +4,8 @@ valley <- read_csv("plotting/changes.csv")
 
 # seven day avg of new confirmed cases
 
-ggplot(valley) + geom_line(aes(x=date, y=avg, color=county), size = 1.5) + scale_color_manual(values = c("#1f77b4", "#2CA02C", "#FF7F0E", "#d62728")) + xlab("day") + ylab("Average new confirmed cases") +  ggtitle("Average new confirmed COVID cases (over past 7 days)") + theme_light()
+valley$county <- factor(valley$county, levels = c("Berkshire", "Hampden", "Franklin", "Hampshire"))
+
+ggplot(valley) + geom_line(aes(x=date, y=avg, color=county), size = 1.5) + scale_color_manual(values = c("#1f77b4", "#FF7F0E", "#2CA02C", "#d62728")) + xlab("day") + ylab("Average new confirmed cases (over past 7 days)") +  ggtitle("Average new confirmed cases by county") + theme_light()
 
 ggsave("site/changes.png")
