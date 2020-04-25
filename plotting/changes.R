f543e450bfaebb40db39c4f59799cfa75ca82233
Abe Handler <akh2103@gmail.com>
2020-04-25 12:11:12 -0400

    just backup

--- plotting/changes.R
+++ plotting/changes.R
@@ -6,10 +6,10 @@ valley <- read_csv("plotting/changes.csv")
 
 valley$county <- factor(valley$county, levels = c("Berkshire", "Hampden", "Franklin", "Hampshire"))
 
-ggplot(valley) + geom_line(aes(x=date, y=avg, color=county), size = 1.5) + scale_color_manual(values = c("#1f77b4", "#FF7F0E", "#2CA02C", "#d62728")) + ylab("Avg. new cases (past 7 days)") + xlab("") + theme_minimal() + facet_grid(cols = vars(county)) + theme(text = element_text(size=11), strip.background = element_blank(),
+ggplot(valley) + geom_line(aes(x=date, y=avg, color=county), size = 1.5) + scale_color_manual(values = c("#1f77b4", "#FF7F0E", "#2CA02C", "#d62728")) + ylab("Avg. new cases (past 7 days)") + xlab("") + theme_minimal() + facet_grid(cols = vars(county)) + theme(legend.position = "none", text = element_text(size=10), strip.background = element_blank(),
    strip.text.x = element_blank()) + scale_x_date(date_breaks = "3 weeks", date_labels = "%m-%d")
 
-width = 5
+width = 4
 height = (9/16) * width
 
 ggsave("site/changes.png", width = width, height=height)
