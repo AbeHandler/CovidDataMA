3257a972bf38643ac54827c542d308062b324d82
Abe Handler <akh2103@gmail.com>
2020-04-25 12:26:25 -0400

    just backup

--- plotting/changes.R
+++ plotting/changes.R
@@ -14,8 +14,7 @@ theme_minimal() +
 facet_grid(cols = vars(county)) + 
 scale_x_date(date_breaks = "3 weeks", date_labels = "%m-%d") + 
 ggtitle('Avg. # new confirmed cases, per county') + 
-theme(legend.position = "none", text = element_text(size=10), strip.background = element_blank(),
-   strip.text.x = element_blank())
+theme(legend.position = "none", text = element_text(size=10), strip.background = element_blank())
 
 width = 4
 height = (9/16) * width
