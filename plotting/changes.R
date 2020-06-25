a603b667690bb617d09c9ffe53a22a99f9619ead
Abe Handler <akh2103@gmail.com>
2020-06-25 07:52:26 -0400

    just backup

--- plotting/changes.R
+++ plotting/changes.R
@@ -12,7 +12,7 @@ ylab("7-day average") +
 xlab("") + 
 theme_minimal() + 
 facet_grid(cols = vars(county)) + 
-scale_x_date(date_breaks = "3 weeks", date_labels = "%m-%d") + 
+scale_x_date(date_labels=c("%m/%d","%m/%d"), breaks=c(as.Date("2020-05-01"), Sys.Date())) + 
 ggtitle('Avg. # new confirmed cases') + 
 theme(legend.position = "none", text = element_text(size=10), plot.title = element_text(hjust = 0.5), strip.background = element_blank())
 
